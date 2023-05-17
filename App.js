7ad27a845cbe86c8e5c028614ffe728cb900a389
Singhnav13 <navjot131@gmail.com>
2023-05-17 23:02:40 +0530

    First Commit

---
+++ App.js
@@ -0,0 +1,12 @@
+const heading = React.createElement("h1", {id: "Parent"}, 
+[ React.createElement("h2", {id: "h2"}, 
+[ React.createElement("h3", {id: "h3"}, "I am Child"),
+React.createElement("h3", {id: "h3"}, "I am Child")]),
+
+React.createElement("h2", {id: "h2"}, 
+[ React.createElement("h3", {id: "h3"}, "I am Child"),
+React.createElement("h3", {id: "h3"}, "I am Child")])]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+
