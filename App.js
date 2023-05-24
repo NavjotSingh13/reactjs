15017d40249880207b7d0957fd6eec6e0c4c6078
Singhnav13 <navjot131@gmail.com>
2023-05-24 13:55:22 +0530

    Header added to the website

--- App.js
+++ App.js
@@ -2,26 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-const Heading2 =()=>  (
-    <div id="container">
-        <h1>This is Heading 2</h1>
-     </div>
-)
+
+/* 
+
+Header
+-Logo
+-Nav Items
+
+Body
+-Search
+-Restaurant Container
+  -Restaurant Cards
+
+Footer
+-Copyright
+-Address
+-Contact
 
 
+*/
 
-const abc = 1000;
 
-const Heading = () => (
-    <div id="container">
-        {Heading2()}
-        <Heading2></Heading2>
-        <h1>{abc}</h1>
-        <h1>This is Functional component</h1>
-     </div>
-)
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img 
+          className="logo" 
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" />
+      </div>
+      <div className='nav-items'>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Heading />);
+root.render(<AppLayout />);
 
