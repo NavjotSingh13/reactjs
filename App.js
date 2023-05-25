641399abd7a4e7ec0810769812138bf4a19ba290
Singhnav13 <navjot131@gmail.com>
2023-05-26 00:43:51 +0530

    props passed to the components

--- App.js
+++ App.js
@@ -43,10 +43,39 @@ const Header = () => {
   )
 }
 
+const Restaurantcard = (props) => {
+  return (
+    <div className="restaurant-card" style={{  backgroundColor: "#f0f0f0" }}>
+      <img
+       className="restaurant-logo"
+       alt = "restaurant-logo"
+       src="https://b.zmtcdn.com/data/pictures/3/20669503/144fc8254779fbb0df44c72c8dc1075b_o2_featured_v2.jpg?output-format=webp">
+      </img>
+      <h3>{props.restaurantname}</h3>
+      <h4>{props.cuisine}</h4>
+      <h4>{props.ratings}</h4>
+      <h4>{props.deliverytime}</h4>
+    </div>
+  )
+}
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="restaurant-container">
+        <Restaurantcard restaurantname="KFC" cuisine="Paneer" ratings="4.9 stars" deliverytime="50 minutes" />
+        <Restaurantcard restaurantname="Pizzahut" cuisine="Pizza" ratings="4.9 stars" deliverytime="50 minutes" />
+      </div>
+    </div>
+  )
+}
+
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body />
     </div>
   )
 }
