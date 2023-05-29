597bd854b7a33865550f6d145ecc75098b1c949d
Singhnav13 <navjot131@gmail.com>
2023-05-29 22:53:22 +0530

    Filtered items having ratings less than 4 using useState hooj

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,58 @@
 import RestaurantCard from "./RestaurantCard";
 import restaurantlist from "./utils/mockData";
+import { useState } from "react";
+
+
+// let restaurantlist = [
+//   {
+//     key: "1",
+//     image: "https://b.zmtcdn.com/data/pictures/3/20669503/144fc8254779fbb0df44c72c8dc1075b_o2_featured_v2.jpg?output-format=webp",
+//     restaurantname: "KFC",
+//     cuisine: "Paneer",
+//     ratings: "4.8",
+//     deliverytime: "40 minutes",
+//     price: 400
+//   },
+//   {
+//     key: "2",
+//     image: "https://b.zmtcdn.com/data/pictures/7/18535157/429ec6ddd1350367aae4aea48a031ece_o2_featured_v2.jpg?output-format=webp",
+//     restaurantname: "KFC",
+//     cuisine: "Paneer",
+//     ratings: "3.8",
+//     deliverytime: "40 minutes",
+//     price: 400  
+//   },
+//   {
+//     key: "3",
+//     image: "https://b.zmtcdn.com/data/pictures/3/20669503/144fc8254779fbb0df44c72c8dc1075b_o2_featured_v2.jpg?output-format=webp",
+//     restaurantname: "KFC",
+//     cuisine: "Paneer",
+//     ratings: "4.8",
+//     deliverytime: "40 minutes",
+//     price: 400
+//   }
+// ];
+
 
 const Body = () => {
-    return (
-      <div className="body">
-        <div className="search">Search</div>
-        <div className="restaurant-container">
-          {restaurantlist.map((restaurant) => (
-            <RestaurantCard key={restaurant.key} restaurantData={restaurant} />
-          ))}
-        </div>
+  const [listOfRestaurant, setlistOfRestaurant] = useState(restaurantlist);
+  return (
+    <div className="body">
+      <div className="filter">
+        <button className="filter-btn" onClick={() => {
+          const filteredList = listOfRestaurant.filter(
+            (res) => res.ratings > 4
+          );
+          setlistOfRestaurant(filteredList);
+        }}>Top Rated Restaurant</button>
       </div>
-    )
-  }
+      <div className="restaurant-container">
+        {listOfRestaurant.map((restaurant) => (
+          <RestaurantCard key={restaurant.key} restaurantData={restaurant} />
+        ))}
+      </div>
+    </div>
+  )
+}
 
-  export default Body;
+export default Body;
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,27 +1,27 @@
 const RestaurantCard = (props) => {
-    const { restaurantData } = props;
-    const { 
-      image, 
-      restaurantname, 
-      cuisine,
-      ratings, 
-      deliverytime, 
-      price 
-    } = restaurantData;
-    return (
-      <div className="restaurant-card" style={{ backgroundColor: "#f0f0f0" }}>
-        <img
-          className="restaurant-logo"
-          alt="restaurant-logo"
-          src={restaurantData.image}>
-        </img>
-        <h3>{restaurantname}</h3>
-        <h4>{cuisine}</h4>
-        <h4>{ratings}</h4>
-        <h4>{deliverytime}</h4>
-        <h4>₹{price}</h4>
-      </div>
-    )
-  }
+  const { restaurantData } = props;
+  const {
+    image,
+    restaurantname,
+    cuisine,
+    ratings,
+    deliverytime,
+    price
+  } = restaurantData;
+  return (
+    <div className="restaurant-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="restaurant-logo"
+        alt="restaurant-logo"
+        src={restaurantData.image}>
+      </img>
+      <h3>{restaurantname}</h3>
+      <h4>{cuisine}</h4>
+      <h4>{ratings}</h4>
+      <h4>{deliverytime}</h4>
+      <h4>₹{price}</h4>
+    </div>
+  )
+}
 
-  export default RestaurantCard;
+export default RestaurantCard;
